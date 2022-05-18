561a019e326e29ce05321b44044a42d37ea18b8b
Roshan Dev-Business Partner <Roshan.Dev-External@alfuttaim.com>
2022-05-18 17:25:26 +0400

    completed service test case

--- test/service.test.js
+++ test/service.test.js
@@ -1,16 +1,43 @@
 const service = require('../service');
 
-describe('Get Alphabet Array', ()=>{
-    it('When there is no input', ()=>{
-        expect(service.getAlphabetArray).toThrow('Input is required')
-    })
-    it('When there is Invalid input', ()=>{
-        expect(() => service.getAlphabetArray('&')).toThrow('Invalid Input')
-    })
-    it('When there is input A', ()=>{
-        expect(service.getAlphabetArray('A')).toHaveLength(1)
-    })
-    it('When there is input D', ()=>{
-        expect(service.getAlphabetArray('D')).toHaveLength(4)
-    })
-})
+describe('Get Alphabet Array', () => {
+  it('When there is no input', () => {
+    expect(service.getAlphabetArray).toThrow('Input is required');
+  });
+  it('When there is Invalid input', () => {
+    expect(() => service.getAlphabetArray('&')).toThrow('Invalid Input');
+  });
+  it('When there is input A', () => {
+    const array = service.getAlphabetArray('A');
+    expect(array).toHaveLength(1);
+    expect(array).toEqual(['A']);
+  });
+  it('When there is input D', () => {
+    const array = service.getAlphabetArray('D');
+    expect(array).toHaveLength(4);
+    expect(array).toEqual(['A', 'B', 'C', 'D']);
+  });
+});
+
+describe('Get Space', () => {
+  it('When input is 4', () => {
+    expect(service.getSpace(6)).toHaveLength(6);
+  });
+});
+
+describe('Check Algo logic', () => {
+  it('When input array contains A-D', () => {
+    const input = ['A', 'B', 'C', 'D'];
+    const output = service.algoLogic(input);
+    const toMatch = [
+        '    A',
+        '   B  B',
+        '  C    C',
+        ' D      D',
+        '  C    C',
+        '   B  B',
+        '    A'
+];
+    expect(output).toEqual(toMatch);
+  });
+});
