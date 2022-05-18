adaa68daaa678ead992e2b4965640e0c606cee7d
Roshan Dev-Business Partner <Roshan.Dev-External@alfuttaim.com>
2022-05-18 16:24:16 +0400

    init

---
+++ diamond.js
@@ -0,0 +1,5 @@
+const alphabets = require('./alphabet.json');
+
+const k = 'D';
+
+console.log(alphabets.slice(0, alphabets.indexOf('D') + 1));
---
+++ helper.js
@@ -0,0 +1,5 @@
+module.exports = {
+    getAlphabetArray : alphabet => {
+        if(!alphabet) throw 
+    }
+}
